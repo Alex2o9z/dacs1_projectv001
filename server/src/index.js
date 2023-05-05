6fe61cd0a1e402b5e73caeb95411ab2ed9abcc68
Alex2o9z <hothangtrinh200902@gmail.com>
2023-05-05 15:18:48 +0700

    connected with MongoDB and created server/auth routers

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose"
+import {} from "dotenv/config";
+import { userRouter } from "../routes/users.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+
+// Connect to mongodb
+const URI = process.env.MONGODB_URL;
+mongoose.connect(URI).catch(error => console.log(error));
+
+app.listen(process.env.SERVER_PORT, ()=> console.log("(from server/src/index.js) SERVER STARTED on port "+process.env.SERVER_PORT));
