27ee0bedd977ea209f3312f2bb8493962dd6f018
Alex2o9z <hothangtrinh200902@gmail.com>
2023-05-12 18:29:19 +0700

    mobile complete home screen step

--- mobile/components/index.js
+++ mobile/components/index.js
@@ -1,4 +1,4 @@
-import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn";
+import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn.jsx";
 
 // home screen
 import Welcome from "./home/welcome/Welcome";
@@ -6,11 +6,11 @@ import Nearbyjobs from "./home/nearby/Nearbyjobs";
 import Popularjobs from "./home/popular/Popularjobs";
 
 // job details screen
-import Company from "./jobdetails/company/Company";
-import { default as JobTabs } from "./jobdetails/tabs/Tabs";
-import { default as JobAbout } from "./jobdetails/about/About";
-import { default as JobFooter } from "./jobdetails/footer/Footer";
-import Specifics from "./jobdetails/specifics/Specifics";
+import Company from "./jobdetails/company/Company.jsx";
+import { default as JobTabs } from "./jobdetails/tabs/Tabs.jsx";
+import { default as JobAbout } from "./jobdetails/about/About.jsx";
+import { default as JobFooter } from "./jobdetails/footer/Footer.jsx";
+import Specifics from "./jobdetails/specifics/Specifics.jsx";
 
 // common
 import NearbyJobCard from "./common/cards/nearby/NearbyJobCard";
