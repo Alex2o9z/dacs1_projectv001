108812ad7fd76de60bdc9fec094c9bacd8264ad6
Alex2o9z <hothangtrinh200902@gmail.com>
2023-05-13 16:54:47 +0700

    mobile complete useFetch Hook (API intergration) step

--- mobile/components/home/popular/Popularjobs.jsx
+++ mobile/components/home/popular/Popularjobs.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
 import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
 import { useRouter } from "expo-router";
 
 import styles from './popularjobs.style';
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard.jsx";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error } = useFetch(
+    'search',
+    {
+      query: 'React developer',
+      num_pages: "1"
+    }
+  );
 
   return (
     <View style={styles.container}>
@@ -27,11 +32,13 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
-            renderItem={({item}) => (
-              <PopularJobCard item={item} />
+            data={data}
+            keyExtractor={(item) => item.job_id}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+              />
             )}
-            keyExtractor={item => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
           />
