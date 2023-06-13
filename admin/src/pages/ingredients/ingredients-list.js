6dec646d81bf19d061eb39385fc1321feb9b1926
Alex2o9z <hothangtrinh200902@gmail.com>
2023-06-14 01:16:22 +0700

    admin completed add ingredient n recipe

--- admin/src/pages/ingredients/ingredients-list.js
+++ admin/src/pages/ingredients/ingredients-list.js
@@ -4,45 +4,56 @@ import axios from 'axios';
 import { useGetUserID } from '../../hooks/useGetUserID.js';
 import { useCookies } from 'react-cookie';
 import { AiOutlinePlus, AiOutlineEye, AiOutlineEdit } from "react-icons/ai";
-import { Card, Typography } from "@material-tailwind/react";
+import { Card, Typography, Button } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 
 const URI = process.env.REACT_APP_SERVER_URL;
 const MAX_DESCRIPTION_LENGTH = 50;
+const PER_PAGE = 5;
 
 export const IngredientsList = () => {
 	const userID = useGetUserID();
 	const [ingredients, setIngredients] = useState([]);
-	// const [savedIngredients, setSavedIngredients] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(0);
 	const [cookies, _] = useCookies(["access_token"]);
 
 	useEffect(() => {
-		const fetchIngredient = async () => {
-			try {
-				const response = await axios.get(URI + "/admin/ingredients");
-				setIngredients(response.data);
-			} catch (error) {
-				console.error(error);
-			}
-		};
-		fetchIngredient();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+		fetchIngredients(currentPage);
+	}, [currentPage]);
+
+	const fetchIngredients = async (page) => {
+		try {
+			const response = await axios.get(`${URI}/admin/ingredients?page=${page}&limit=${PER_PAGE}`);
+			setIngredients(response.data.ingredients);
+			setTotalPages(response.data.totalPages);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
 	const TABLE_HEAD = ["#", "Tên", "Ảnh", "Mô tả", "Thao tác"];
 
-	const TABLE_ROWS = ingredients?.map((ingredient) => ({
-        id: ingredient._id,
-        name: ingredient.name,
+	const TABLE_ROWS = ingredients?.map((ingredient, index) => ({
+		id: ingredient._id,
+		name: ingredient.name,
 		description: ingredient.description,
 		imageUrl: ingredient.imageUrl,
-    })) || [];
+	})) || [];
 
 	const truncateDescription = (description) => {
 		if (description.length <= MAX_DESCRIPTION_LENGTH) {
-		  return description;
+			return description;
 		}
 		return `${description.slice(0, MAX_DESCRIPTION_LENGTH)}...`;
+	};
+
+	const handlePrevPage = () => {
+		setCurrentPage((prevPage) => prevPage - 1);
+	};
+
+	const handleNextPage = () => {
+		setCurrentPage((prevPage) => prevPage + 1);
 	};
 
 	return (
@@ -78,11 +89,11 @@ export const IngredientsList = () => {
 						</tr>
 					</thead>
 					<tbody>
-						{TABLE_ROWS.map(({  id, name, description, imageUrl}, index) => (
+						{TABLE_ROWS.map(({ id, name, description, imageUrl }, index) => (
 							<tr key={name} className="even:bg-blue-gray-50/50 hover:bg-orange-100">
 								<td className="p-4">
 									<Typography variant="small" color="blue-gray" className="font-normal">
-										{index + 1}
+										{(currentPage - 1) * PER_PAGE + index + 1}
 									</Typography>
 								</td>
 								<td className="p-4">
@@ -113,6 +124,153 @@ export const IngredientsList = () => {
 					</tbody>
 				</table>
 			</Card>
+
+			{/* Pagination */}
+			<div className="flex justify-center items-center mt-4">
+				<Button
+					color="deep-orange"
+					buttonType="filled"
+					size="regular"
+					rounded={true}
+					className="mr-2 p-2"
+					onClick={handlePrevPage}
+					disabled={currentPage === 1}
+				>
+					Trước
+				</Button>
+				<div>
+					Trang {currentPage} / {totalPages}
+				</div>
+				<Button
+					color="deep-orange"
+					buttonType="filled"
+					size="regular"
+					rounded={true}
+					className="ml-2 p-2"
+					onClick={handleNextPage}
+					disabled={currentPage === totalPages}
+				>
+					Sau
+				</Button>
+			</div>
 		</div>
 	);
 };
+
+
+// import { React } from "react";
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { useGetUserID } from '../../hooks/useGetUserID.js';
+// import { useCookies } from 'react-cookie';
+// import { AiOutlinePlus, AiOutlineEye, AiOutlineEdit } from "react-icons/ai";
+// import { Card, Typography } from "@material-tailwind/react";
+// import { Link } from "react-router-dom";
+
+// const URI = process.env.REACT_APP_SERVER_URL;
+// const MAX_DESCRIPTION_LENGTH = 50;
+
+// export const IngredientsList = () => {
+// 	const userID = useGetUserID();
+// 	const [ingredients, setIngredients] = useState([]);
+// 	const [cookies, _] = useCookies(["access_token"]);
+
+// 	useEffect(() => {
+// 		const fetchIngredient = async () => {
+// 			try {
+// 				const response = await axios.get(URI + "/admin/ingredients");
+// 				setIngredients(response.data);
+// 			} catch (error) {
+// 				console.error(error);
+// 			}
+// 		};
+// 		fetchIngredient();
+// 	}, []);
+
+// 	const TABLE_HEAD = ["#", "Tên", "Ảnh", "Mô tả", "Thao tác"];
+
+// 	const TABLE_ROWS = ingredients?.map((ingredient) => ({
+//         id: ingredient._id,
+//         name: ingredient.name,
+// 		description: ingredient.description,
+// 		imageUrl: ingredient.imageUrl,
+//     })) || [];
+
+// 	const truncateDescription = (description) => {
+// 		if (description.length <= MAX_DESCRIPTION_LENGTH) {
+// 		  return description;
+// 		}
+// 		return `${description.slice(0, MAX_DESCRIPTION_LENGTH)}...`;
+// 	};
+
+// 	return (
+// 		<div className="p-4">
+// 			<div className="py-2 flex justify-between items-center">
+// 				<h1 className='uppercase text-gray-700 text-center md:text-3xl sm:text-2xl'>Danh sách nguyên liệu</h1>
+// 				<button className="bg-[#ff611d] text-white p-2 rounded-lg">
+// 					<Link to="/ingredient-add" className="flex flex-row items-center ">
+// 						<AiOutlinePlus />
+// 						<span className="ml-2">
+// 							Thêm nguyên liệu
+// 						</span>
+// 					</Link>
+// 				</button>
+// 			</div>
+
+// 			{/* Table Data */}
+// 			<Card className="overflow-scroll h-full w-full">
+// 				<table className="w-full min-w-max table-auto text-left">
+// 					<thead>
+// 						<tr>
+// 							{TABLE_HEAD.map((head) => (
+// 								<th key={head} className="border-b border-deep-orange-100 bg-deep-orange-400 p-4">
+// 									<Typography
+// 										variant="small"
+// 										color="white"
+// 										className="font-bold leading-none"
+// 									>
+// 										{head}
+// 									</Typography>
+// 								</th>
+// 							))}
+// 						</tr>
+// 					</thead>
+// 					<tbody>
+// 						{TABLE_ROWS.map(({  id, name, description, imageUrl}, index) => (
+// 							<tr key={name} className="even:bg-blue-gray-50/50 hover:bg-orange-100">
+// 								<td className="p-4">
+// 									<Typography variant="small" color="blue-gray" className="font-normal">
+// 										{index + 1}
+// 									</Typography>
+// 								</td>
+// 								<td className="p-4">
+// 									<Typography variant="small" color="blue-gray" className="font-bold">
+// 										{name}
+// 									</Typography>
+// 								</td>
+// 								<td className="p-4">
+// 									<Typography variant="small" color="blue-gray" className="font-normal">
+// 										<img src={imageUrl} alt={name} className="w-fit h-[50px] object-cover rounded-t-lg" />
+// 									</Typography>
+// 								</td>
+// 								<td className="p-4">
+// 									<Typography variant="small" color="blue-gray" className="font-normal">
+// 										{truncateDescription(description)}
+// 									</Typography>
+// 								</td>
+// 								<td className="p-4">
+// 									<button className="mr-2 p-2 bg-blue-400 text-white rounded-lg">
+// 										<AiOutlineEye />
+// 									</button>
+// 									<button className="p-2 bg-deep-orange-400 text-white rounded-lg">
+// 										<AiOutlineEdit />
+// 									</button>
+// 								</td>
+// 							</tr>
+// 						))}
+// 					</tbody>
+// 				</table>
+// 			</Card>
+// 		</div>
+// 	);
+// };
--- server/routes/ingredients.js
+++ server/routes/ingredients.js
@@ -8,11 +8,24 @@ import { verifyToken } from './users.js';
 
 const router = express.Router();
 
-// Fetch all ingredients for admin ingredient list page API request
+// Fetch paginated ingredients for admin ingredient list page API request
 router.get("/", async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
     try {
-        const response = await IngredientModel.find({});
-        res.json(response);
+        const count = await IngredientModel.countDocuments();
+        const totalPages = Math.ceil(count / limit);
+        const skip = (page - 1) * limit;
+
+        const ingredients = await IngredientModel.find({})
+            .skip(skip)
+            .limit(limit);
+
+        res.json({
+            ingredients,
+            totalPages,
+        });
     } catch (error) {
         res.json(error);
     }
@@ -25,17 +38,18 @@ router.get("/create", async (req, res) => {
         const ingredientTypes = await IngredientTypeModel.find({});
         const regions = await RegionModel.find({});
         const response = {
-            "healths": healths,
-            "ingredientTypes": ingredientTypes,
-            "regions": regions,
+            healths,
+            ingredientTypes,
+            regions,
         }
         res.json(response);
     } catch (error) {
         res.json(error);
     }
 });
+
 router.post("/create", verifyToken, async (req, res) => {
-    const ingredient = new IngredientModel(req.body,);
+    const ingredient = new IngredientModel(req.body);
 
     try {
         const response = await ingredient.save();
@@ -45,40 +59,90 @@ router.post("/create", verifyToken, async (req, res) => {
     }
 });
 
-// // Add RecipeID to User API request
-// router.put("/", verifyToken, async (req, res) => {
-//     try {
-//         const recipe = await RecipeModel.findById(req.body.recipeID);
-//         const user = await UserModel.findById(req.body.userID);
-//         user.savedRecipes.push(recipe);
-//         await user.save();
-//         res.json({ savedRecipes: user.savedRecipes });
-//     } catch (error) {
-//         res.json(error);
-//     }
-// });
-
-// //  API request
-// router.get("/savedRecipes/ids/:userID", async (req, res) => {
-//     try {
-//         const user = await UserModel.findById(req.params.userID);
-//         res.json({ savedRecipes: user?.savedRecipes });
-//     } catch (error) {
-//         res.json(error);
-//     }
-// });
-
-// //  API request
-// router.get("/savedRecipes/:userID", async (req, res) => {
-//     try {
-//         const user = await UserModel.findById(req.params.userID);
-//         const savedRecipes = await RecipeModel.find({
-//             _id: { $in: user.savedRecipes },
-//         });
-//         res.json({ savedRecipes });
-//     } catch (error) {
-//         res.json(error);
-//     }
-// });
-
 export { router as ingredientRouter };
+
+
+// import express from "express";
+// import mongoose from "mongoose";
+// import { HealthModel } from "../models/Healths.js";
+// import { IngredientTypeModel } from "../models/IngredientTypes.js";
+// import { RegionModel } from "../models/Regions.js";
+// import { IngredientModel } from "../models/Ingredients.js";
+// import { verifyToken } from './users.js';
+
+// const router = express.Router();
+
+// // Fetch all ingredients for admin ingredient list page API request
+// router.get("/", async (req, res) => {
+//     try {
+//         const response = await IngredientModel.find({});
+//         res.json(response);
+//     } catch (error) {
+//         res.json(error);
+//     }
+// });
+
+// // Add new ingredients API request
+// router.get("/create", async (req, res) => {
+//     try {
+//         const healths = await HealthModel.find({});
+//         const ingredientTypes = await IngredientTypeModel.find({});
+//         const regions = await RegionModel.find({});
+//         const response = {
+//             "healths": healths,
+//             "ingredientTypes": ingredientTypes,
+//             "regions": regions,
+//         }
+//         res.json(response);
+//     } catch (error) {
+//         res.json(error);
+//     }
+// });
+// router.post("/create", verifyToken, async (req, res) => {
+//     const ingredient = new IngredientModel(req.body,);
+
+//     try {
+//         const response = await ingredient.save();
+//         res.json(response);
+//     } catch (error) {
+//         res.json(error);
+//     }
+// });
+
+// // // Add RecipeID to User API request
+// // router.put("/", verifyToken, async (req, res) => {
+// //     try {
+// //         const recipe = await RecipeModel.findById(req.body.recipeID);
+// //         const user = await UserModel.findById(req.body.userID);
+// //         user.savedRecipes.push(recipe);
+// //         await user.save();
+// //         res.json({ savedRecipes: user.savedRecipes });
+// //     } catch (error) {
+// //         res.json(error);
+// //     }
+// // });
+
+// // //  API request
+// // router.get("/savedRecipes/ids/:userID", async (req, res) => {
+// //     try {
+// //         const user = await UserModel.findById(req.params.userID);
+// //         res.json({ savedRecipes: user?.savedRecipes });
+// //     } catch (error) {
+// //         res.json(error);
+// //     }
+// // });
+
+// // //  API request
+// // router.get("/savedRecipes/:userID", async (req, res) => {
+// //     try {
+// //         const user = await UserModel.findById(req.params.userID);
+// //         const savedRecipes = await RecipeModel.find({
+// //             _id: { $in: user.savedRecipes },
+// //         });
+// //         res.json({ savedRecipes });
+// //     } catch (error) {
+// //         res.json(error);
+// //     }
+// // });
+
+// export { router as ingredientRouter };
